bf7fc7f3e66df575a00fb90b5c826adb38f58935
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-25 14:35:52 +0200

    add /login endpoint, fix /method endpoint

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from hashlib import sha256
+
+from fastapi import FastAPI, HTTPException, Request, Response
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
 app = FastAPI()
+app.secret_key = "ba217dd867bf9b31ca568c533cc0ecacb3c2d9e12d94cfca8731abc593eda237"
 
 
 @app.get("/")
@@ -9,29 +13,27 @@ def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get("/welcome")
+@app.api_route(path="/welcome", methods=["GET", "POST"])
 def welcome():
     return {"message": "Hello"}
 
 
-@app.get("/method")
-def method():
-    return {"method": "GET"}
+@app.post("/login")
+def login(login: str, password: str, response: Response):
+    cred = sha256(bytes(f"trudnYPaC13Nt{app.secret_key}", encoding="utf8")).hexdigest()
+    session_token = sha256(
+        bytes(f"{login}{password}{app.secret_key}", encoding="UTF-8")
+    ).hexdigest()
+
+    response.set_cookie(key="session_token", value=session_token)
+    if session_token != cred:
+        raise HTTPException(status_code=403, detail="Unauthorised")
+    return RedirectResponse("/welcome")
 
 
-@app.post("/method")
-def method():
-    return {"method": "POST"}
-
-
-@app.put("/method")
-def method():
-    return {"method": "PUT"}
-
-
-@app.delete("/method")
-def method():
-    return {"method": "DELETE"}
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
 
 
 class Patient(BaseModel):
