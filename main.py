0bf078c02793f30e03ddb95331c4082a3fe6f85b
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2020-04-29 18:45:27 +0200

    add /albums endpoints

--- main.py
+++ main.py
@@ -154,3 +154,42 @@ async def tracks_composers(composer_name: str):
     if len(songs_names) == 0:
         raise HTTPException(status_code=404, detail={"error": "Not Found"})
     return songs_names
+
+
+class Album(BaseModel):
+    title: str
+    artist_id: int
+
+
+@app.post("/albums")
+async def albums(response: Response, album: Album):
+    cursor = app.db_connection.cursor()
+    cursor.row_factory = lambda cursor, row: row[0]
+    artist = cursor.execute(
+        "SELECT artistid FROM artists WHERE artistid = ?", (album.artist_id,)
+    ).fetchone()
+    if not artist:
+        raise HTTPException(status_code=404, detail={"error": "Not Found"})
+
+    album = cursor.execute(
+        "INSERT INTO albums (title, artistid) VALUES (?,?)",
+        (album.title, int(artist),),
+    )
+    app.db_connection.commit()
+    new_album = album.lastrowid
+    album = app.db_connection.execute(
+        "SELECT albumid, title, artistid FROM albums WHERE albumid = ?", (new_album,),
+    ).fetchone()
+    response.status_code = 201
+    return album
+
+
+@app.get("/albums/{album_id}")
+async def album(album_id):
+    cursor = app.db_connection.cursor()
+    album = cursor.execute(
+        "SELECT albumid, title, artistid FROM albums WHERE albumid = ?", (album_id,),
+    ).fetchone()
+    if not album:
+        raise HTTPException(status_code=404, detail={"error": "Not Found"})
+    return album
